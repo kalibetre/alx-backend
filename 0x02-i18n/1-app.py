92d3d2fae5227f6054af91b5c5699cf440180535
kalibetre <kalbetre@icloud.com>
2023-04-25 21:39:12 +0300

    update app config from object

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -10,11 +10,11 @@ babel = Babel(app)
 class Config:
     """A Config app class"""
     LANGUAGES = ["en", "fr"]
-    DEFAULT_TZ = "UTC"
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app.config['BABEL_DEFAULT_LOCALE'] = Config.LANGUAGES[0]
-app.config['BABEL_DEFAULT_TIMEZONE'] = Config.DEFAULT_TZ
+app.config.from_object(Config)
 
 
 @app.route('/')
