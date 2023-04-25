61c2891c4838d83bd17e1dcd5ab076d0d285900c
kalibetre <kalbetre@icloud.com>
2023-04-25 21:23:08 +0300

    remove strickt slash

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def welcome() -> str:
     """Renders a message"""
     return render_template('0-index.html')
