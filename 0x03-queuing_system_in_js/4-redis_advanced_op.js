28baaba4515e9dfaf5c12dd630206b6a79f19ee0
kalibetre <kalbetre@icloud.com>
2023-05-23 14:32:48 +0300

    add documentation

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -6,16 +6,36 @@ const redisClient = createClient();
 const hsetAsync = promisify(redisClient.hset).bind(redisClient);
 const hgetallAsync = promisify(redisClient.hgetall).bind(redisClient);
 
+/**
+ * Asynchronously adds a key-value pair to a hash table.
+ *
+ * @param {any} key - the key to be added
+ * @param {any} value - the value to be associated with the key
+ * @return {Promise<string>} - a promise resolving to a string representation of the reply
+ */
 async function addToHashTable(key, value) {
     const reply = await hsetAsync(HASH_KEY, key, value);
     print(`Reply: ${reply}`);
 }
 
+/**
+ * Asynchronously retrieves all the values of the hash stored at the specified
+ * key and logs them to the console.
+ *
+ * @return {Promise<Object>} A promise that resolves to an object containing
+ * all the field-value pairs of the hash.
+ */
 async function displayHashTable() {
     const reply = await hgetallAsync(HASH_KEY);
     console.log(reply);
 }
 
+/**
+ * Executes the main function which adds various key-value pairs to a hash table
+ * and displays the current state of the hash table.
+ *
+ * @return {Promise<void>} Does not return anything.
+ */
 async function main() {
     await addToHashTable('Portland', 50);
     await addToHashTable('Seattle', 80);
