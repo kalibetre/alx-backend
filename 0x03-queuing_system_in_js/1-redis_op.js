1dc694b8e972790580a5a3d14732c0c11609c12b
kalibetre <kalbetre@icloud.com>
2023-05-23 13:52:34 +0300

    add functions to set and display values from redis server

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,41 @@
+import { createClient, print } from 'redis';
+
+const redisClient = createClient();
+
+redisClient.on('error', (err) => {
+    console.log(`Redis client not connected to the server: ${err}`);
+});
+
+redisClient.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
+/**
+ * Sets a new school value in Redis and displays a confirmation using
+ * redis.print
+ *
+ * @param {string} schoolName - the name of the school to set the value for
+ * @param {string} value - the value to set for the school
+ * @return {void}
+ */
+function setNewSchool(schoolName, value) {
+    redisClient.set(schoolName, value, (err, reply) => {
+        print(`Reply: ${reply}`);
+    });
+}
+
+/**
+ * Displays the value of a given school name from Redis.
+ *
+ * @param {string} schoolName - The name of the school to retrieve from Redis.
+ * @return {undefined} This function does not return a value.
+ */
+function displaySchoolValue(schoolName) {
+    redisClient.get(schoolName, (err, reply) => {
+        console.log(reply);
+    });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
